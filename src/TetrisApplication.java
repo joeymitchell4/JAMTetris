5dc36d4a04a679fba6748a0869659c430ea9184d
mihinton <hintonmichael80@yahoo.com>
2017-03-26 14:39:48 -0400

    Renamed launch java file name Added TetrisBoard.java

---
+++ src/TetrisApplication.java
@@ -0,0 +1,29 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class TetrisApplication extends JFrame {
+	private static final long serialVersionUID = -4951055018923259413L;
+
+	public TetrisApplication() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new TetrisBoard());
+
+        setSize(700, 700);
+
+        setTitle("Tetris");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }    
+    
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+            	TetrisApplication tetrisApp = new TetrisApplication();
+                tetrisApp.setVisible(true);
+            }
+        });
+    }
+}
---
+++ src/TetrisBoard.java
@@ -0,0 +1,9 @@
+import javax.swing.JPanel;
+
+public class TetrisBoard extends JPanel {
+	private static final long serialVersionUID = -2768315633419131803L;
+
+	public TetrisBoard() {
+		
+	}
+}
