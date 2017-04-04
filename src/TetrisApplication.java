a7573cb03a8414b7478fc49bd2a6af640016a945
mihinton <hintonmichael80@yahoo.com>
2017-04-04 16:20:05 -0400

    Added block images

--- src/TetrisApplication.java
+++ src/TetrisApplication.java
@@ -1,29 +1,37 @@
 import java.awt.EventQueue;
+import java.io.IOException;
+
 import javax.swing.JFrame;
 
 public class TetrisApplication extends JFrame {
 	private static final long serialVersionUID = -4951055018923259413L;
 
 	public TetrisApplication() {
-        initUI();
-    }
+		initUI();
+	}
 
-    private void initUI() {
-        add(new TetrisBoard());
+	private void initUI() {
+		try {
+			add(new TetrisBoard());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-        setSize(700, 700);
+		setSize(700, 700);
+		setResizable(false); // CANNOT RESIZE GAME
+		
+		setTitle("Tetris");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
 
-        setTitle("Tetris");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-    }    
-    
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-            	TetrisApplication tetrisApp = new TetrisApplication();
-                tetrisApp.setVisible(true);
-            }
-        });
-    }
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				TetrisApplication tetrisApp = new TetrisApplication();
+				tetrisApp.setVisible(true);
+			}
+		});
+	}
 }
--- src/TetrisBoard.java
+++ src/TetrisBoard.java
@@ -1,9 +1,17 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class TetrisBoard extends JPanel {
 	private static final long serialVersionUID = -2768315633419131803L;
 
-	public TetrisBoard() {
+	public TetrisBoard() throws IOException {
+		JLabel pic = new JLabel(new ImageIcon(ImageIO.read(new File("block_images/yellow.png"))));
 		
+		this.add(pic);
 	}
 }
