f8203d2f6bbb7ffa3413267627131ed8106ca270
mihinton <hintonmichael80@yahoo.com>
2017-04-04 16:49:53 -0400

    Able to show image on JPanel Added KeyListener for arrow presses

--- src/TetrisBoard.java
+++ src/TetrisBoard.java
@@ -1,3 +1,5 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
 
@@ -10,8 +12,45 @@ public class TetrisBoard extends JPanel {
 	private static final long serialVersionUID = -2768315633419131803L;
 
 	public TetrisBoard() throws IOException {
-		JLabel pic = new JLabel(new ImageIcon(ImageIO.read(new File("block_images/yellow.png"))));
-		
+		JLabel pic = new JLabel(new ImageIcon(ImageIO.read(new File(
+				"src/images/darkblue.png"))));
 		this.add(pic);
+
+		this.setFocusable(true);
+		this.requestFocusInWindow();
+
+		this.addKeyListener(new KeyListener() {
+			
+			public void keyTyped(KeyEvent e) {
+
+			}
+
+			public void keyReleased(KeyEvent e) {
+
+			}
+
+			public void keyPressed(KeyEvent e) {
+				// CODE INSPIRED BY: http://stackoverflow.com/a/617004
+				int keyCode = e.getKeyCode();
+				switch (keyCode) {
+				case KeyEvent.VK_UP:
+					System.out.println("UP");
+					// handle up
+					break;
+				case KeyEvent.VK_DOWN:
+					System.out.println("DOWN");
+					// handle down
+					break;
+				case KeyEvent.VK_LEFT:
+					System.out.println("LEFT");
+					// handle left
+					break;
+				case KeyEvent.VK_RIGHT:
+					System.out.println("RIGHT");
+					// handle right
+					break;
+				}
+			}
+		});
 	}
 }
